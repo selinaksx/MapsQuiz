5910c49dce37fdec21a70d446aebcb80c3787d7e
Shelin <skusmiawati@gmail.com>
2017-11-21 07:14:33 +0700

    Maps Getting Started

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/mapsquiz/MainActivity.java
@@ -0,0 +1,88 @@
+package id.sch.smktelkom_mlg.learn.mapsquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap m_map;
+    boolean mapReady = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMap = (Button) findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mapReady)
+                    m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
+        });
+
+        Button btnSatellite = (Button) findViewById(R.id.btnSatellite);
+        btnSatellite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mapReady)
+                    m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            }
+        });
+
+        Button btnHybrid = (Button) findViewById(R.id.btnHybrid);
+        btnHybrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mapReady)
+                    m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            }
+        });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Map Ready!", Toast.LENGTH_SHORT);
+        toast.show();
+        mapReady = true;
+        m_map = map;
+        LatLng smktelkom = new LatLng(-7.976830, 112.658958);
+        CameraPosition target = CameraPosition.builder().target(smktelkom).zoom(14).build();
+        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
